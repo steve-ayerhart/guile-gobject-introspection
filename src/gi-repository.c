aaec086d42dd4a576839fb73de3d7986fe39aa50
Steve Erhart <steve@ayerh.art>
2018-05-22 23:16:07 -0400

    repo functions added

--- src/gi-repository.c
+++ src/gi-repository.c
@@ -8,10 +8,10 @@ SCM_DEFINE (scm_g_irepository_get_default, "g-i-repository-get-default", 0, 0, 0
             (),
             "")
 {
-  SCM repository = scm_make_smob (repository_t);
-  SCM_SET_SMOB_DATA (repository, g_irepository_get_default ());
+  SCM scm_repository = scm_make_smob (repository_t);
+  SCM_SET_SMOB_DATA (scm_repository, g_irepository_get_default ());
 
-  return repository;
+  return scm_repository;
 }
 
 // FIXME: scm_Flags is currently ignored
@@ -24,6 +24,7 @@ SCM_DEFINE (scm_g_irepository_require, "g-i-repository-require", 1, 4, 0,
   GTypelib *typelib;
   GError *error;
   const char *version;
+  const char *namespace;
   SCM scm_typelib;
 
   repo = (GIRepository *) SCM_SMOB_DATA (scm_repository);
@@ -39,6 +40,9 @@ SCM_DEFINE (scm_g_irepository_require, "g-i-repository-require", 1, 4, 0,
     version = scm_to_locale_string (scm_version);
 
   error = NULL;
+
+  if (scm_is_symbol (scm_namespace))
+    scm_namespace = scm_symbol_to_string (scm_namespace);
 
   typelib = g_irepository_require (repo,
                                    scm_to_locale_string (scm_namespace),
@@ -57,10 +61,74 @@ SCM_DEFINE (scm_g_irepository_require, "g-i-repository-require", 1, 4, 0,
   return scm_typelib;
 }
 
+SCM_DEFINE (scm_g_irepository_get_n_infos, "g-i-repository-get-n-infos", 1, 1, 0,
+            (SCM scm_namespace, SCM scm_repository),
+            ""
+            )
+{
+  GIRepository *repo;
+
+  repo = (GIRepository *) SCM_SMOB_DATA (scm_repository);
+
+  if (SCM_UNBNDP (scm_repository))
+    repo = NULL;
+  else
+    repo = g_irepository_get_default ();
+
+  if (scm_is_symbol (scm_namespace))
+    scm_namespace = scm_symbol_to_string (scm_namespace);
+
+  return scm_from_int (g_irepository_get_n_infos (repo,
+                                                  scm_to_locale_string (scm_namespace)));
+}
+
+SCM_DEFINE (scm_g_irepository_find_by_name, "g-i-repository-find-by-name", 2, 1, 0,
+           (SCM scm_namespace, SCM scm_name, SCM scm_repository),
+           ""
+           )
+{
+  GIRepository *repo;
+  GIBaseInfo *info;
+  GError *error;
+  SCM scm_info;
+
+  repo = (GIRepository *) SCM_SMOB_DATA (scm_repository);
+
+  error = NULL;
+
+  if (SCM_UNBNDP (scm_repository))
+    repo = NULL;
+  else
+    repo = g_irepository_get_default ();
+
+  if (scm_is_symbol (scm_namespace))
+    scm_namespace = scm_symbol_to_string (scm_namespace);
+
+  if (scm_is_symbol (scm_name))
+    scm_name = scm_symbol_to_string (scm_name);
+
+  info = g_irepository_find_by_name (repo,
+                                     scm_to_locale_string (scm_namespace),
+                                     scm_to_locale_string (scm_name));
+
+  if (info == NULL)
+    return SCM_UNSPECIFIED;
+
+  scm_info = scm_make_smob (base_info_t);
+  SCM_SET_SMOB_DATA (scm_info, info);
+
+  return scm_info;
+}
+
+
+
 void
 gi_repository_init (void)
 {
   #ifndef SCM_MAGIC_SNARFER
   #include "gi-repository.x"
   #endif
+
+  repository_t = scm_make_smob_type ("g-i-repository", 0);
+  typelib_t = scm_make_smob_type ("g-typelib", 0);
 }
