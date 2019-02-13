77bbeeb50abf5c21160d9b6141bb7743c72502ac
Steve Ayerhart <steve.ayerhart@dudesolutions.com>
2019-02-12 22:18:10 -0500

    WIP reorg scheme

--- src/module/gir/info.scm
+++ src/module/gir/info.scm
@@ -1,60 +1,11 @@
 (define-module (gir info)
-  #:use-module (gir g-object g-type)
+  #:use-module (gobject gtype)
 
   #:use-module (oop goops)
   #:use-module (system foreign)
   #:use-module (system foreign-object)
 
-  #:export (<base-info>
-            get-name
-            <callable-info>
-            <function-info>
-            <signal-info>
-            <v-func-info>
-            <registered-type-info>
-            get-g-type
-            <enum-info>
-            <interface-info>
-            <object-info>
-            get-methods
-            <struct-info>
-            <union-info>
-            <arg-info>
-            <constant-info>
-            get-value
-            <field-info>
-            <property-info>
-            <type-info>))
 
-(define ggi-lib "/home/steve/Source/guile-gobject-introspection/src/.libs/gobject-introspection")
-
-(eval-when (expand load eval)
-  (dynamic-call "ggi_base_info_init"
-                (dynamic-link "/home/steve/Source/guile-gobject-introspection/src/.libs/gobject-introspection")))
-
-(define-class <callable-info> (<base-info>))
-(define-class <callback-info> (<callable-info>))
-(define-class <function-info> (<callable-info>))
-(define-class <signal-info> (<callable-info>))
-(define-class <v-func-info> (<callable-info>))
-
-(define-class <registered-type-info> (<base-info>))
-(define-class <enum-info> (<registered-type-info>))
-(define-class <interface-info> (<registered-type-info>))
-(define-class <object-info> (<registered-type-info>))
-(define-class <struct-info> (<registered-type-info>))
-(define-class <union-info> (<registered-type-info>))
-
-(define-class <arg-info> (<base-info>))
-(define-class <constant-info> (<base-info>))
-(define-class <field-info> (<base-info>))
-(define-class <property-info> (<base-info>))
-(define-class <type-info> (<base-info>))
-(define-class <value-info> (<base-info>))
-
-(eval-when (expand load eval)
-  (dynamic-call "ggi_infos_init"
-                (dynamic-link ggi-lib)))
 
 ;; helper to generate the boilerplace for definine methods which take only the info
 ;; example:
@@ -66,45 +17,3 @@
 ;;     (%g-base-info-get-name base-info)))
 ;;   (define-method (get-type (base-info <base-info>)
 ;;     (%g-base-info-get-type base-info))))
-(define-syntax define-info-methods
-  (λ (stx)
-    (syntax-case stx ()
-      ((_  info-name (method-name))
-       (with-syntax
-           ((class-name (datum->syntax stx (string->symbol
-                                             (string-append
-                                              "<"
-                                               (symbol->string (syntax->datum #'info-name))
-                                               ">"))))
-            (ffi-name (datum->syntax stx (string->symbol
-                                          (string-append
-                                            "%g-"
-                                            (symbol->string (syntax->datum #'info-name))
-                                            "-"
-                                            (symbol->string (syntax->datum #'method-name)))))))
-         #'(begin
-             (define-method (method-name (info-name class-name))
-               (ffi-name info-name))
-             (export method-name))))
-      ((_ info-name (method-name0 method-name* ...))
-       #'(begin
-           (define-info-methods info-name (method-name0))
-           (define-info-methods info-name (method-name* ...)))))))
-
-;;; BaseInfo
-(define-info-methods base-info
-  (get-name))
-
-;;; RegisteredTypeInfo
-(define-info-methods registered-type-info
-  (get-g-type))
-
-;;; ObjectInfo
-
-(define-info-methods object-info
-  (get-methods))
-
-;;; ConstantInfo
-
-(define-info-methods constant-info
-  (get-value))
--- src/module/gir/repository.scm
+++ src/module/gir/repository.scm
@@ -1,29 +1,118 @@
 (define-module (gir repository)
-  #:use-module (gir info)
-  #:use-module (gir g-object utils)
-  #:use-module (gir g-object g-type)
+  #:use-module (glib utils)
+  #:use-module (gobject gtype)
 
   #:use-module (oop goops)
   #:use-module (system foreign)
 
-  #:export (repository-get-default
+  #:export (get-default
             find-by-g-type
             require
-            get-infos))
+            get-infos
+            <gi-base-info>
+            get-name
+            <gi-callable-info>
+            <gi-function-info>
+            <gi-callback-info>
+            <gi-signal-info>
+            <gi-v-func-info>
+            <gi-registered-type-info>
+            get-g-type
+            <gi-enum-info>
+            <gi-interface-info>
+            <gi-object-info>
+            get-methods
+            <gi-struct-info>
+            <gi-union-info>
+            <gi-arg-info>
+            <gi-constant-info>
+            get-value
+            <gi-field-info>
+            <gi-property-info>
+            <gi-type-info>))
+
+(define gi-lib "/home/steve/Source/guile-gobject-introspection/src/.libs/gobject-introspection")
 
 (eval-when (expand load eval)
-  (dynamic-call "ggi_repository_init"
+  (dynamic-call "gi_base_info_init"
                 (dynamic-link "/home/steve/Source/guile-gobject-introspection/src/.libs/gobject-introspection")))
+
+(define-class <gi-callable-info> (<gi-base-info>))
+(define-class <gi-callback-info> (<gi-callable-info>))
+(define-class <gi-function-info> (<gi-callable-info>))
+(define-class <gi-signal-info> (<gi-callable-info>))
+(define-class <gi-v-func-info> (<gi-callable-info>))
+
+(define-class <gi-registered-type-info> (<gi-base-info>))
+(define-class <gi-enum-info> (<gi-registered-type-info>))
+(define-class <gi-interface-info> (<gi-registered-type-info>))
+(define-class <gi-object-info> (<gi-registered-type-info>))
+(define-class <gi-struct-info> (<gi-registered-type-info>))
+(define-class <gi-union-info> (<gi-registered-type-info>))
+
+(define-class <gi-arg-info> (<gi-base-info>))
+(define-class <gi-constant-info> (<gi-base-info>))
+(define-class <gi-field-info> (<gi-base-info>))
+(define-class <gi-property-info> (<gi-base-info>))
+(define-class <gi-type-info> (<gi-base-info>))
+(define-class <gi-value-info> (<gi-base-info>))
 
 (eval-when (expand load eval)
-  (dynamic-call "ggi_base_info_init"
+  (dynamic-call "gi_infos_init"
                 (dynamic-link "/home/steve/Source/guile-gobject-introspection/src/.libs/gobject-introspection")))
 
-(define-method (require (repository <repository>) (namespace <symbol>))
-  (%g-i-repository-require repository namespace))
+(define-syntax define-info-methods
+  (λ (stx)
+    (syntax-case stx ()
+      ((_  info-name (method-name))
+       (with-syntax
+           ((class-name (datum->syntax stx (string->symbol
+                                             (string-append
+                                              "<"
+                                               (symbol->string (syntax->datum #'info-name))
+                                               ">"))))
+            (ffi-name (datum->syntax stx (string->symbol
+                                          (string-append
+                                            "%g-"
+                                            (symbol->string (syntax->datum #'info-name))
+                                            "-"
+                                            (symbol->string (syntax->datum #'method-name)))))))
+         #'(begin
+             (define-method (method-name (info-name class-name))
+               (ffi-name info-name))
+             (export method-name))))
+      ((_ info-name (method-name0 method-name* ...))
+       #'(begin
+           (define-info-methods info-name (method-name0))
+           (define-info-methods info-name (method-name* ...)))))))
+
+;;; BaseInfo
+(define-info-methods base-info
+  (get-name))
+
+;;; RegisteredTypeInfo
+(define-info-methods registered-type-info
+  (get-g-type))
+
+;;; ObjectInfo
+
+(define-info-methods object-info
+  (get-methods))
+
+;;; ConstantInfo
+
+(define-info-methods constant-info
+  (get-value))
+
+;;; GIRepository
+
+(eval-when (expand load eval)
+  (dynamic-call "gi_repository_init"
+                (dynamic-link "/home/steve/Source/guile-gobject-introspection/src/.libs/gobject-introspection"))) (define-method (require (repository <repository>) (namespace <symbol>))
+  (%g-irepository-require repository namespace))
 
 (define-method (get-infos (repository <repository>) (namespace <symbol>))
-  (%g-i-repository-get-infos repository namespace))
+  (%g-irepository-get-infos repository namespace))
 
 (define-method (find-by-g-type (repository <repository>) (g-type <real>))
-  (%g-i-repository-find-by-g-type repository g-type))
+  (%g-irepository-find-by-g-type repository g-type))
